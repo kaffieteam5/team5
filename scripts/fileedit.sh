b9f6c12c6bfe63673b1c27c6005b4e62205262a0
kaffyzenomi <kaffydip@gmail.com>
2023-03-12 03:12:00 +0000

    my first commit

---
+++ scripts/fileedit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#This script will edit the ebun file within the ubuntu home directory
+sed -i 's/child/daugther/g' ../ebun
---
+++ scripts/readtest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#this is use to demo echo and read functions
+read -p "Please enter your name: " name 
+echo "welcome to Etech Devops Master class, $name"
